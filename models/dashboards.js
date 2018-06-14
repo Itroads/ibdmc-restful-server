b211735d0500da36e61e73c46450c91b24508e9e
Joker <740199246@qq.com>
2018-06-14 14:55:41 +0800

    dashboard model

--- models/dashboards.js
+++ models/dashboards.js
@@ -18,8 +18,8 @@ const dashboardSchema = new Schema({
         i: { type: String, required: true },
         componentList: { type: Array, required: true },
         bgstyle: {
-          bgcolorstyle: { type: String, default: null },
-          bgimagestyle: { type: String, default: null }
+          bgcolorstyle: { type: String, default: "" },
+          bgimagestyle: { type: String, default: "" }
         },
       }
     ],
