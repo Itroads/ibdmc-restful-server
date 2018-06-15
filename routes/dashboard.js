f23b49aa863eb8389c7341f77719be1978f93e27
Joker <740199246@qq.com>
2018-06-15 16:44:13 +0800

    纠正 dashboard route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -37,6 +37,6 @@ router.post('/upload', upload.single('bgImage'), dashboardController.uploadImage
  * Restful api:  /dashboard/delete
  * Description:  通过名字，判断是否已经存在
 */
-router.post('/delete', dashboardController.deteleRecentImage)
+router.post('/delete', dashboardController.deleteDashboard)
 
 export default router
