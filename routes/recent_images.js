d74fa378a6cb0466381d6d7a682698b26d6e2b1d
Joker <740199246@qq.com>
2018-06-28 17:27:15 +0800

    上传图片，重命名

--- routes/recent_images.js
+++ routes/recent_images.js
@@ -5,9 +5,29 @@ import multer from 'multer'
 import recentImageController from '../controller/recent_images'
 
 const router = express.Router()
-const upload = multer({
-  dest: './upload'
+
+const storage = multer.diskStorage({
+  // dest: './upload'
+  destination: './upload',
+  filename: (req, file, cb) => {
+    let extend = ''
+    switch (file.mimetype) {
+      case 'image/png':
+        extend = '.png'
+        break;
+      case 'image/jpeg':
+        extend = '.jpeg'
+        break;
+      case 'image/gif':
+        extend = '.gif'
+        break;
+      default:
+        break;
+    }
+    cb(null, file.fieldname + '-' + Date.now() + extend)
+  }
 })
+const upload = multer({ storage: storage })
 
 /*
  * Restful api:  /recent_image/list
@@ -25,6 +45,6 @@ router.get('/delete/:id/:filename', recentImageController.deteleRecentImage)
  * Restful api:  /recent_image/upload
  * Description:  通过名字，判断是否已经存在
 */
-router.post('/upload', upload.single('bgImage'), recentImageController.uploadImage)
+router.post('/upload', upload.single('ibdmc'), recentImageController.uploadImage)
 
 export default router
